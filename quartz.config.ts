13a65a53a4c6cacbdd8b9c0b0c57dcce1c76f64c
Choji Kaseda <kasedac@aga-ye.com>
2026-05-08 22:38:16 +0900

    chore: update baseUrl to www.aga-ye.com for separate production deployment

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "ja-JP",
-    baseUrl: "quartz-playground.pages.dev",
+    baseUrl: "www.aga-ye.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
