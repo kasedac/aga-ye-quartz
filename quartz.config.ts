ea33ae4375ee51d3307d578cb80b4e79a2a12128
Choji Kaseda <kasedac@aga-ye.com>
2026-05-08 23:10:28 +0900

    chore: update pageTitle to アガ・エ

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz Playground",
+    pageTitle: "アガ・エ",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
